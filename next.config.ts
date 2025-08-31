333647c2068e41b0a0dbae81d53802a892fdc08a
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2025-08-31 12:41:51 +0900

    tailwind 10

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
-// next.config.ts
-
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/* next.config.mjs */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
-        hostname: "images.unsplash.com",
+        protocol: "https",
+        hostname: "fakestoreapi.com",
       },
     ],
   },
