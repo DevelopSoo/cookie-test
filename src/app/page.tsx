ff7cd2b3efb906edd26e54b3fda24535d5c1b5a6
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2025-09-11 13:39:51 +0900

    배포 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,42 @@
+// 프론트엔드 코드
 "use client";
 
-import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    const getCookie = async () => {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/cookie`, {
-        // 요청 시 쿠키 포함
-        credentials: "include",
-      });
-      const data = await res.json();
-      console.log("쿠키: ", data);
-    };
-    getCookie();
-  }, []);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const response = await fetch("http://localhost:4000/api/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
 
   return (
     <div>
-      <h1>쿠키 세팅</h1>
-      <button
-        onClick={() => {
-          const setCookie = async () => {
-            const res = await fetch(
-              `${process.env.NEXT_PUBLIC_API_URL}/api/cookie`,
-              {
-                method: "POST",
-                // 요청 시 쿠키 포함
-                credentials: "include",
-              },
-            );
-            const data = await res.json();
-            console.log("쿠키: ", data);
-          };
-          setCookie();
-        }}
-      >
-        토큰을 쿠키에 저장하게 하기
-      </button>
+      <h1>로그인 페이지</h1>
+      <input
+        type="text"
+        placeholder="아이디"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="비밀번호"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>로그인</button>
+      <a href="http://localhost:5173">링크 유도</a>
     </div>
   );
 }
